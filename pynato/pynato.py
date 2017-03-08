498237944aeb8e82c0d8c3e375108e1beaee6021
Lorenzo Bivens <lorenzobivens@gmail.com>
2017-03-08 00:11:39 -0800

    indentation

--- pynato/pynato.py
+++ pynato/pynato.py
@@ -1,13 +1,13 @@
 class Pynato:
     abc = { "A":"Alpha", "B":"Bravo", "C":"Charlie", "D":"Delta", 
-                      "E":"Echo", "F":"Foxtrot", "G":"Golf", "H":"Hotel", 
-                      "I":"India", "J":"Juliet", "K":"Kilo", "L":"Lima", 
-                      "M":"Mike", "N":"November", "O":"Oscar", "P":"Papa", 
-                      "Q":"Quebec", "R":"Romeo", "S":"Sierra", "T":"Tango", 
-                      "U":"Uniform", "V":"Victor", "W":"Whiskey", "X":"Xray", 
-                      "Y":"Yankee", "Z":"Zulu", "0":"Zero", "1":"One", 
-                      "2":"Two", "3":"Three", "4":"Four", "5":"Five",
-                      "6":"Six", "7":"Seven", "8":"Eight", "9":"Niner"} 
+            "E":"Echo", "F":"Foxtrot", "G":"Golf", "H":"Hotel", 
+            "I":"India", "J":"Juliet", "K":"Kilo", "L":"Lima", 
+            "M":"Mike", "N":"November", "O":"Oscar", "P":"Papa", 
+            "Q":"Quebec", "R":"Romeo", "S":"Sierra", "T":"Tango", 
+            "U":"Uniform", "V":"Victor", "W":"Whiskey", "X":"Xray", 
+            "Y":"Yankee", "Z":"Zulu", "0":"Zero", "1":"One", 
+            "2":"Two", "3":"Three", "4":"Four", "5":"Five",
+            "6":"Six", "7":"Seven", "8":"Eight", "9":"Niner"} 
 
     def __init__(self, message): 
         self.message = message
